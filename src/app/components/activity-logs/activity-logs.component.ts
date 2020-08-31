d005002fda984a406d1ca0f9581ea50caba9c745
carlitos <carmoicast@gmail.com>
2020-08-31 00:42:10 -0500

    Implementar primer metodo para obtener todos activity logs

--- src/app/components/activity-logs/activity-logs.component.ts
+++ src/app/components/activity-logs/activity-logs.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivityLogsService } from '../../services/activity-logs.service';
 
 @Component({
   selector: 'app-activity-logs',
   templateUrl: './activity-logs.component.html',
-  styleUrls: [ ]
+  styleUrls: [ ],
+  providers: [ ActivityLogsService ]
 })
 export class ActivityLogsComponent implements OnInit {
 
-  constructor() { }
+  activity_logs:             any[] = [];
+
+  constructor( public _activity_logs_service_yts: ActivityLogsService) {
+    this._activity_logs_service_yts = _activity_logs_service_yts;
+   }
 
   ngOnInit() {
+    this.getAll();
   }
+
+
+  getAll( ) {
+    return this._activity_logs_service_yts.getAllActivityLogs()
+    .then( activity_logs => {
+      console.log('activity logs: ', activity_logs);
+
+      this.activity_logs = activity_logs;
+    })
+    .catch(function (error) {
+      return ;
+    });
+  }
+
 
 }

4981e8f79cb15c7d9a6377050913c3eb0fd92ef2
carlitos <carmoicast@gmail.com>
2020-08-30 22:34:48 -0500

    Agregar componente de activity logs

---
+++ src/app/components/activity-logs/activity-logs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-logs',
+  templateUrl: './activity-logs.component.html',
+  styleUrls: [ ]
+})
+export class ActivityLogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
